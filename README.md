2bbcec43d9ae4fff9713c8ef8b5ed828fc9305b0
The Goose <ryanshaffer1324@gmail.com>
2024-07-07 11:44:01 -0400

    Worked out assignment and edited readme.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # svg-logo-maker
 
-**Note**: There is no starter code for this assignment.
+*Note*: There is no starter code for this assignment.
 
 ### User Story
 
@@ -28,8 +28,33 @@ AND the output text "Generated logo.svg" is printed in the command line
 WHEN I open the `logo.svg` file in a browser
 THEN I am shown a 300x200 pixel image that matches the criteria I entered
 ```
+## Creation process
+First step was making the remote repo and then cloning that to my local device. From there, I established my file structure to be alike that of which the class provided. With structure established, it was time to copy provided snippets and the user story/acceptance criteria. Later on, I did add more to the structure and also a generateSVG file for the sake of handling the making of a svg file based on the data gathered from user inputs.
 <br/>
-some snippets of code were pulled from the homework assignemnt from the week prior. <br/>
-Also from the following link 
+I realized early on that this project was much like the readme generator assignment. With that, I knew I could copy some snippets of code and edit them to befit this assignment. 
+<br/>
+I also gained inspiration from the following video and links: 
+<br/>
+https://youtu.be/GJYMcLus3v0?si=FVBjnHC9ST-bKgHU
 <br/>
 https://github.com/ThomasCalle/Thomas-Object-Oriented-Programming-SVG-Logo-Maker/blob/main/index.js
+<br/>
+https://github.com/ThomasCalle/Thomas-Object-Oriented-Programming-SVG-Logo-Maker/blob/main/lib/shapes.test.js
+<br/>
+https://github.com/ThomasCalle/Thomas-Object-Oriented-Programming-SVG-Logo-Maker/blob/main/lib/shapes.js
+
+## Challenges 
+The main challenge I encountered during this project was the writing of the tests. We spent only a day on that subject in class and with the minimal exposure and time to practice, I found myself relying heavily on examples. I found a great video to watch that covered this exact topic (listed above) and that really helped break down the blockages so I could move forward expediently. 
+
+## Github/contact info
+Account: https://github.com/shyanrafer
+<br/>
+REPO: https://github.com/shyanrafer/svg-logo-maker
+<br/>
+Email: ryanshaffer1324@gmail.com (preferred method of contact)
+
+## Installation/use guide
+Open your CLI, navigate to desired file destination, simply clone the repo in your desired file location, open the repo in your preferred code editor, once there - open the packages.json folder in integrated terminal, run npm install, close terminal once packs have installed, open the index.js file in your integrated terminal, run node index, fulfill prompts, after prompts have been fulfilled an svg file will be generated. *Note* Use of same file name will overwrite pre-existing files with that name. 
+
+## Contribution
+Create a feature branch and edit there. After, push up for review. 
--- index.js
+++ index.js
@@ -1,117 +1,61 @@
-const fs = require('fs')
+// import necessary pacaks and files
+const fs = require('fs');
 const inquirer = require("inquirer");
-const generateSvg = require('./lib/JavaScript/generateSVG')
-const { Circle, Square, Triangle } = require("./lib/JavaScript/shapes");
+const generateSVG = require('./lib/JavaScript/generateSVG');
 
-// Imports the graceful-fs, inquirer, Circle, Square, and Triangle modules.
-// Defines a Svg class that has a constructor with three methods for rendering and setting the text and shape elements in the SVG string.
-
-class Svg {
-    constructor(){
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render(){
-
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    } // position text and then apply inputted text and color to text
-    setTextElement(text,color){
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    } // apply inputted shape
-    setShapeElement(shape){
-        this.shapeElement = shape.render()
-
-    }
-    
-}
-
-// Defines array of 'questions' using the 'inquirer' library with the following questions.
-// Each question is an object that specifies the properties of TEXT, TEXT COLOR, SHAPE COLOR, and Pixel Image.
+// questions array to be prompted later
 const questions = [
-	  {
-      name: "title",
-			message: "Enter the title of your svg file."
-		},
     {
-        name: "text",
-        message: "TEXT: Enter up to (3) Characters:",
+        name: "title",
+        message: "Enter the title of your svg file:"
     },
     {
-        name: "text-color",
-        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):",
+        name: "text",
+        message: "TEXT: Enter up to (3) Characters:"
+    },
+    {
+        name: "fontColor",
+        message: "TEXT COLOR: Enter a color keyword (OR a hexadecimal number):"
     },
     {
         name: "shapeColor",
-        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):",
+        message: "SHAPE COLOR: Enter a color keyword (OR a hexadecimal number):"
     },
     {
         type: "list",
         name: "imgShape",
-        message: "Choose which Pixel Image you would like?",
-        choices: ["Circle", "Square", "Triangle"],
-    },
+        message: "Choose which shape you would like:",
+        choices: ["Circle", "Square", "Triangle"]
+    }
 ];
 
-// Function to write data to file
-// function writeToFile(fileName, data) {
-// 	console.log("Writing [" + data + "] to file [" + fileName + "]")
-//     fs.writeFile(fileName.svg, data, function (err) {
-//         if (err) {
-//             return console.log(err);
-//         }
-//         console.log("Kuddos, you generated an SVG file!");
-//     });
-// }
-
-
-// TODO: Create a function to initialize app
-
-
-// Pulled from https://github.com/ThomasCalle/Thomas-Object-Oriented-Programming-SVG-Logo-Maker/blob/main/index.js for inspiration
-// async function init() {
-// 	let svgString = ""; 
-
-//     // Prompt the user for answers
-//     const answers = await inquirer.prompt(questions);
-
-
-
-// 	// Create a new Svg instance and add the shape and text elements to it
-// 	const svg = new Svg();
-// 	svg.setTextElement(user_text, user_font_color);
-// 	svg.setShapeElement(user_shape);
-// 	svgString = svg.render();
-	
-// 	//Print shape to log
-// 	console.log("Displaying shape:\n\n" + svgString);
-// 	document.getElementById("svg_image").innerHTML = svgString;
-
-// 	console.log("Shape generation complete!");
-// 	console.log("Writing shape to file...");
-// 	writeToFile(svg_file, svgString); 
-// }
-// init()
-
-// Since this is a lot like last week's assigntment I pulled the following and adapted for this project
-function writeToFile(fileName, data) { 
-  fs.writeFile(`${fileName}.svg`, data, err => err? console.log("Error!") : console.log("File Written!") )
-
+// writeToFile fx copied from last week and adapted for this assignment
+function writeToFile(fileName, data) {
+    fs.writeFile(`${fileName}.svg`, data, err => {
+        if (err) {
+            console.log("Error!");
+        } else {
+            console.log("SVG file written!");
+        }
+    });
 }
 
 
-// Since this is a lot like last week's assigntment I pulled the following and adapted for this project
-function init() {
-  // call inquirer, use the prompt method with questions as the param.
-  inquirer
-    .prompt(questions)
-    // then use the answers 
-    .then((answers) => {
-      console.log("Task complete")
-      console.log(answers);
-      const sv_g = generateSvg(answers);
-      console.log(sv_g)
-      // once we have data, write to file
-      writeToFile(answers.title, sv_g);
-    })
+// basis for this snippet was gathered from https://github.com/ThomasCalle/Thomas-Object-Oriented-Programming-SVG-Logo-Maker/blob/main/index.js
+async function init() {
+    try {
+        // assign the inputs from user to answers
+        const answers = await inquirer.prompt(questions);
+
+        // create the content with generateSVG and answers as param
+        const svgContent = generateSVG(answers);
+        if (svgContent) { // if there is content, writes file
+            // uses title from answers to write the file name, uses the content from svg to write the file content
+            writeToFile(answers.title, svgContent);
+        }
+    } catch (error) { // otherwise logs an error
+        console.error('An error occurred:', error);
+    }
 }
-init()
+
+init();
